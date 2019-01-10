0f2651737df2ef4e9842d6a2d136375090a332d9
Addison Moore <addisondrewmoore@gmail.com>
2019-01-10 12:00:10 -0800

    setup file structure

---
+++ src/containers/ProfileContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ProfileContainer extends Component {
+  render() {
+    return (
+      <div className="Profile">
+        <h1>User Profile</h1>
+      </div>
+    );
+  }
+}
+
+export default ProfileContainer;
