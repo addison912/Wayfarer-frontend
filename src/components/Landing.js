7588bc42a4317542e2483debf5caeff49bc857b3
dogtownoak <david.brandon.ford@gmail.com>
2019-01-12 17:54:37 -0800

    updated landing page

---
+++ src/components/Landing.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <div className="slider">
+        </div>
+        <section>
+            <h1>Wayfarer is...</h1>
+            <article>
+                <h2>Topic One</h2>
+                <p>Use what you see, don't plan it. If it's not what you want - stop and change it. Don't just keep going and expect it will get better. This is an example of what you can do with just a few things, a little imagination and a happy dream in your heart.</p>
+            </article>
+            <article>
+                <h2>Topic Two</h2>
+                <p>I really recommend you use odorless thinner or your spouse is gonna run you right out into the yard and you'll be working by yourself. Be brave. You are only limited by your imagination. Imagination is the key to painting. With practice comes confidence. Just think about these things in your mind and drop em' on canvas.
+</p>
+            </article>
+            <article>
+                <h2>Topic Three</h2>
+                <p>Sometimes you learn more from your mistakes than you do from your masterpieces. This is where you take out all your hostilities and frustrations. It's better than kicking the puppy dog around and all that so. And maybe, maybe, maybe...</p>
+            </article>
+        </section>
+
+    </div>
+    );
+}
+}
+
+export default Landing;
--- src/containers/CitiesContainer.js
+++ src/containers/CitiesContainer.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Cities from "../components/Cities";
 import CityDetails from "../components/CityDetails";
+import Landing from "../components/Landing";
 
 class CitiesContainer extends Component {
   render() {
     return (
       <div className="CitiesContainer">
+        <Landing />
         <Cities />
         <CityDetails />
       </div>
