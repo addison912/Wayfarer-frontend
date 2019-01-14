b53ba9e956f9034b68e9a999a40b1372f5024ff7
Addison Moore <addisondrewmoore@gmail.com>
2019-01-14 14:38:50 -0800

    merged and removed redundancies

--- src/containers/ProfileContainer.js
+++ src/containers/ProfileContainer.js
@@ -1,40 +1,37 @@
 import React, { Component } from "react";
-import axios from 'axios';
-import Posts from '../components/Posts'
-
+import axios from "axios";
+import Posts from "../components/Posts";
 
 class ProfileContainer extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-        posts: []
-      }
-  }  
+      posts: []
+    };
+  }
 
-    // componentDidMount () {
-    //   axios.get(`http://localhost:3001/posts/${this.state.userId}`)
-    //           .then(response => {
-    //             this.setState({
-    //               posts: response.data
-    //             })
-    //           })
+  // componentDidMount () {
+  //   axios.get(`http://localhost:3001/posts/${this.state.userId}`)
+  //           .then(response => {
+  //             this.setState({
+  //               posts: response.data
+  //             })
+  //           })
 
-
-    render() 
-    {
-      // let posts = this.state.posts.map((posts, i) => {
-      //   return (
-      //     <div key={i}>
-      //       <Posts info={posts} isLoggedIn={this.state.isLoggedIn}
-      //       />
-      //     </div>
-      //   )
-      // })
+  render() {
+    // let posts = this.state.posts.map((posts, i) => {
+    //   return (
+    //     <div key={i}>
+    //       <Posts info={posts} isLoggedIn={this.state.isLoggedIn}
+    //       />
+    //     </div>
+    //   )
+    // })
 
     return (
       <div className="ProfileContainer">
-      <p>Profile Container</p>
-        {/* <h1>Welcome {this.props.username}</h1>
+        <h1>Welcome {this.props.username}</h1>
+        {/* 
         <img src={this.state.user.profilePic} alt={this.state.user.username} className='dog-image' />
         <p>{this.state.user.username}</p>
         <p>{this.state.user.currentCity}</p>
@@ -44,7 +41,6 @@ class ProfileContainer extends Component {
         <div className="listPosts">
         {posts} */}
       </div>
-  
     );
   }
 }
